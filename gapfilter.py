94163ceaede5975f42775e84e1d734ea03d1f846
ToHanwei <hanwei@shanghaitech.edu.cn>
2020-05-08 23:08:23 +0800

    add delect paramater

--- gapfilter.py
+++ gapfilter.py
@@ -12,6 +12,9 @@ if len(sys.argv) == 1:
 parser = argparse.ArgumentParser(description='FilterGap')
 parser.add_argument('-f', '--MSAfile', help='MSA output file path, as input file')
 parser.add_argument('-o', '--output', help='output file path, filter result')
+parser.add_argument('-d', '--delete', 
+					help='delect this sequence if there is more the set "-d" continues gap',
+					default=10, type=int)
 parser.add_argument('-p', '--precent', 
 					help='Threshold of the percentage of filter site gaps, default=90.0',
 					default=90.0, type=float
@@ -22,6 +25,7 @@ args = parser.parse_args()
 infile = args.MSAfile
 outfile = args.output
 p_gap = args.precent
+n_gap = args.delete
 
 with open(infile) as seqf:
 	seqs = seqf.read().split('>')[1:]
@@ -52,10 +56,12 @@ while True:
 
 seqs_cut = list(zip(*statues_cut))
 seq_cut_list = list(zip(*(seq_list[0], seqs_cut)))
+seq_cut_list = [(name, ''.join(seq_line)) for name, seq_line in seq_cut_list]
+# delect sequence if there is more gaps
+seq_cut_list = [(name, seq_line) for name, seq_line in seq_cut_list if '-'*n_gap not in seq_line]
 
 outf = open(outfile, 'w')
 for name, seq_line in seq_cut_list:
-	seq_line = ''.join(seq_line)
 	line = ">" + name + "\n" + seq_line + '\n'
 	outf.write(line)
 
